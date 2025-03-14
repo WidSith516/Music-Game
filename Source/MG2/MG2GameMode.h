9fefdf93cb0ba06b4e13fed1d7825e1c392df9e0
widsith <widsith@foxmail.com>
2025-03-14 17:54:51 +0800

    Base_Level

---
+++ Source/MG2/MG2GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MG2GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMG2GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMG2GameMode();
+};
+
+
+
